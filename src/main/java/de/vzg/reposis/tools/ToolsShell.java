eca636be1807b06f2c7f15febcce12dc74acaf32
Sebastian Hofmann <sebastian.hofmann@gbv.de>
2025-04-14 10:07:06 +0200

    Initial commit

---
+++ src/main/java/de/vzg/reposis/tools/ToolsShell.java
@@ -0,0 +1,12 @@
+package de.vzg.reposis.tools;
+
+import org.springframework.shell.standard.ShellComponent;
+import org.springframework.shell.standard.ShellOption;
+
+@ShellComponent
+public class ToolsShell {
+
+    public void convertPica(@ShellOption("input") String input, @ShellOption("output") String output) {
+        
+    }
+}
---
+++ src/test/java/de/vzg/reposis/tools/ToolsApplicationTests.java
@@ -0,0 +1,13 @@
+package de.vzg.reposis.tools;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToolsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
